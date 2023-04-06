40adbd7d8d2a56fe0845f4491cd9ceed20087960
youssefsakini <youssef0sakini@gmail.com>
2023-04-06 12:37:49 +0000

    Add tasks details with style

--- src/app/components/tasks-details/tasks-details.component.ts
+++ src/app/components/tasks-details/tasks-details.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TasksItems } from 'src/app/types/TasksItems';
 
 @Component({
   selector: 'app-tasks-details',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./tasks-details.component.scss']
 })
 export class TasksDetailsComponent {
+  rappelItems: TasksItems [] = [
+    {
+      id: '01',
+      name: 'Rappel',
+      icone: 'fa fa-bell'
+    },
+    {
+      id: '02',
+      name: "Ajouter une date d'échéance",
+      icone: 'fa fa-calendar'
+    },
+    {
+      id: '03',
+      name: 'Répéter',
+      icone: 'fa fa-refresh'
+    }
+  ];
+  showMenu = true;
 
+  toggleMenu() {
+    this.showMenu = !this.showMenu;
+    console.log(this.showMenu);
+    
+  }
 }
